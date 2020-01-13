454c3557705ce95ff7b07ab46cb9f1fad97fc98d
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-01-13 22:29:32 +0530

    Update App.js

--- Adopt Me/src/App.js
+++ Adopt Me/src/App.js
@@ -4,6 +4,24 @@ import SearchParams from "./SearchParams";
 
 // your code is going to go here,for now
 const App = () => {
+  // return React.createElement("div", { id: "something-important" }, [
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   React.createElement(Pet, {
+  //     name: "lucy",
+  //     animal: "Dog",
+  //     breed: "labra"
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "kaka",
+  //     animal: "bird",
+  //     breed: "koyal"
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "mussi",
+  //     animal: "Dog",
+  //     breed: "mixed"
+  //   })
+  // ]);
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
