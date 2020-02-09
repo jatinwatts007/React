2bde1b2df9d769e3e403bd140aa849e859bf8eb2
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-02-09 23:05:44 +0530

    Create Results.tsx

---
+++ TypeScript/src/Results.tsx
@@ -0,0 +1,30 @@
+import React, { FunctionComponent } from "react";
+import Pet from "./Pet";
+import { Animal } from "@frontendmasters/pet";
+
+interface IProps {
+  pets: Animal[];
+}
+
+const Results: FunctionComponent<IProps> = ({ pets }) => {
+  return (
+    <div className="search">
+      {pets.length === 0 ? (
+        <h1>No Pets Found</h1>
+      ) : (
+        pets.map(pet => (
+          <Pet
+            animal={pet.type}
+            key={pet.id}
+            name={pet.name}
+            breed={pet.breeds.primary}
+            media={pet.photos}
+            location={`${pet.contact.address.city},${pet.contact.address.state}`}
+            id={pet.id}
+          />
+        ))
+      )}
+    </div>
+  );
+};
+export default Results;
